4c991c0053690f620d0dc60e3d23401c65864c3b
VamsiK21 <121943954+VamsiK21@users.noreply.github.com>
2023-02-16 15:16:33 +0530

    Update index.js
    Here we used separate function for feed back with emoji images container and thank you container and after that we use Conditional rendering inside the render().

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,6 +1,5 @@
 // Write your React code here.
 import {Component} from 'react'
-
 import './index.css'
 
 class Feedback extends Component {
@@ -8,51 +7,72 @@ class Feedback extends Component {
     givenFeedback: false,
   }
 
-  emojiClicked = () => {
+  onClicked = () => {
     this.setState({
       givenFeedback: true,
     })
   }
 
-  render() {
+  renderFeedbackQuestion = () => {
     const {resources} = this.props
-    const {givenFeedback} = this.state
-    const {emojis, loveEmojiUrl} = resources
+    const {emojis} = resources
 
-    console.log(givenFeedback)
+    return (
+      <div className="content-container">
+        <h1 className="heading">
+          How satisfied are you with our customer support performance?
+        </h1>
+        <ul className="emoji-container">
+          {emojis.map(eachItem => (
+            <li key={eachItem.id} className="list-item">
+              <button
+                className="button-img"
+                type="button"
+                onClick={this.onClicked}
+              >
+                <img
+                  className="image"
+                  src={eachItem.imageUrl}
+                  alt={eachItem.name}
+                />
+              </button>
+              <p className="emoji-name">{eachItem.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderThankYouScreen = () => {
+    const {resources} = this.props
+    const {loveEmojiUrl} = resources
+
+    return (
+      <div className="thankyou-container">
+        <img className="image" src={loveEmojiUrl} alt="love emoji" />
+        <h1 className="heading">Thank You</h1>
+        <p className="emoji-name">
+          We will use your feedback to improve our customer service performance
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    const {givenFeedback} = this.state
+
     return (
       <div className="bg-container">
-        {givenFeedback ? (
-          <div className="content-container">
-            <img className="image" src={loveEmojiUrl} />
-            <p className="heading">Thank you</p>
-            <p className="emoji-name">
-              We will use your feedback to improve our customer service
-              performance
-            </p>
-          </div>
-        ) : (
-          <div className="content-container">
-            <h1 className="heading">
-              How satisfied are you with our customer service performance?
-            </h1>
-            <ul className="emoji-container">
-              {emojis.map(eachItem => (
-                <li key={eachItem.id} className="list-item">
-                  <img
-                    onClick={this.emojiClicked}
-                    className="image"
-                    src={eachItem.imageUrl}
-                  />
-                  <p className="emoji-name">{eachItem.name}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+        <div className="feedback-Card">
+          {givenFeedback
+            ? this.renderThankYouScreen()
+            : this.renderFeedbackQuestion()}
+        </div>
       </div>
     )
   }
 }
 
 export default Feedback
+
